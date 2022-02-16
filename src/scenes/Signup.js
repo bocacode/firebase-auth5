ad4fe923b9847c6f8936033148e6aba9aee2a98a
Todd Albert <todd@bocacode.com>
2022-02-16 13:55:50 -0500

    added login and signup with Google

--- src/scenes/Signup.js
+++ src/scenes/Signup.js
@@ -1,19 +1,27 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { app } from '../ConnectAuth'
 
 export default function Signup({ setUser }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth(app);
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    const auth = getAuth(app);
     createUserWithEmailAndPassword(auth, email, password)
       .then(result => {
         setUser(result.user)
-        // navigate to home
+        navigate('/')
+      })
+      .catch(alert)
+  }
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then(result => {
+        setUser(result.user)
         navigate('/')
       })
       .catch(alert)
@@ -35,6 +43,13 @@ export default function Signup({ setUser }) {
         <br />
         <input type="submit" value="Sign up" />
       </form>
+      <button
+        onClick={handleGoogleLogin}
+        style={{
+          backgroundColor: 'black',
+          color: 'white',
+          border: 'none' }}>
+        Sign in with Google</button>
       <p>Already a user? <Link to="/login">Login</Link></p>
     </>
   )
--- src/scenes/Welcome.js
+++ src/scenes/Welcome.js
@@ -1,3 +1,12 @@
-export default function Welcome() {
-  return <h1>Welcome</h1>
+export default function Welcome({ user }) {
+  console.log(user) // email, displayName, photoURL
+  return (
+    <>
+      <h1>Welcome</h1>
+      <h2>{user.displayName || user.email}</h2>
+      {user.photoURL &&
+        <img src={user.photoURL}
+          alt="Profile picture of logged-in user" />}
+    </>
+  )
 }
