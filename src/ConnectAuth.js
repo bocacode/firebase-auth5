c349e4cd40a668facb8e4e631c37aa46959ae7cc
Todd Albert <todd@bocacode.com>
2022-02-16 11:59:12 -0500

    added signin with firebase

---
+++ src/ConnectAuth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpd2-mvBY2zJqMV4hNM52_tXAdijLswb0",
+  authDomain: "fir-auth-bc5.firebaseapp.com",
+  projectId: "fir-auth-bc5",
+  storageBucket: "fir-auth-bc5.appspot.com",
+  messagingSenderId: "81565388915",
+  appId: "1:81565388915:web:a1c578aa01c4c4f9731a06"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
