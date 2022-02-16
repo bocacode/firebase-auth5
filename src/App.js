0eaafd39f7b6a51d43d17d9778b06212666ce0c7
jonathans199 <jso1919@gmail.com>
2022-02-16 16:05:39 -0500

    added sign out and logout button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Login from './scenes/Login';
-import Signup from './scenes/Signup';
-import Welcome from './scenes/Welcome';
+import { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Login from './scenes/Login'
+import Signup from './scenes/Signup'
+import Welcome from './scenes/Welcome'
 
 function App() {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    console.log('here is my useEffect and user', user)
+  }, [])
+
   return (
     <Routes>
-      <Route path="/login" element={<Login setUser={setUser} />} />
-      <Route path="/signup" element={<Signup setUser={setUser} />} />
-      <Route path="/" element={user 
-        ? <Welcome user={user} /> 
-        : <Login setUser={setUser} />} />
+      <Route path='/login' element={<Login setUser={setUser} user={user} />} />
+      <Route path='/signup' element={<Signup setUser={setUser} />} />
+      <Route path='/' element={user ? <Welcome user={user} /> : <Login setUser={setUser} />} />
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/scenes/Login.js
+++ src/scenes/Login.js
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { app } from '../ConnectAuth'
 
-export default function Login({ setUser }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
-  const provider = new GoogleAuthProvider();
-  const auth = getAuth(app);
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
+export default function Login({ setUser, user }) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+  const provider = new GoogleAuthProvider()
+  const auth = getAuth(app)
+
+  useEffect(() => {
+    const localUser = localStorage.getItem('displayName')
+    const avatar = localStorage.getItem('avatar')
+
+    console.log('localUser from LS ', localUser)
+
+    if (localUser) setUser({ ...user, displayName: localUser, photo: avatar })
+  }, [])
+
+  const handleFormSubmit = event => {
+    event.preventDefault()
     signInWithEmailAndPassword(auth, email, password)
       .then(result => {
         setUser(result.user)
@@ -18,39 +28,53 @@ export default function Login({ setUser }) {
       })
       .catch(alert)
   }
+
   const handleGoogleLogin = () => {
     signInWithPopup(auth, provider)
       .then(result => {
         setUser(result.user)
+
+        localStorage.setItem('displayName', result.user.displayName)
+        localStorage.setItem('avatar', result.user.photo)
+        localStorage.setItem('uid', result.user.uid)
+
+        console.log('this is my result', result.user.displayName)
         navigate('/')
       })
       .catch(alert)
   }
+
+  console.log('here is my user from my parent App component', user)
+
   return (
     <>
       <h1>Login</h1>
       <hr />
       <form onSubmit={handleFormSubmit}>
-        <label>Email:
-          <input type="email" value={email}
-            onChange={e => setEmail(e.target.value)} />
+        <label>
+          Email:
+          <input type='email' value={email} onChange={e => setEmail(e.target.value)} />
         </label>
         <br />
-        <label>Password:
-          <input type="password" value={password}
-            onChange={e => setPassword(e.target.value)} />
+        <label>
+          Password:
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
         </label>
         <br />
-        <input type="submit" value="Login" />
+        <input type='submit' value='Login' />
       </form>
       <button
         onClick={handleGoogleLogin}
         style={{
           backgroundColor: 'black',
           color: 'white',
-          border: 'none' }}>
-        Sign in with Google</button>
-      <p>Not a user? <Link to="/signup">Sign Up</Link></p>
+          border: 'none',
+        }}>
+        Sign in with Google
+      </button>
+      <p>
+        Not a user? <Link to='/signup'>Sign Up</Link>
+      </p>
     </>
   )
 }
