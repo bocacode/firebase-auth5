bec9b2f25aef8a4098df516fa1af7897d140aa23
jonathans199 <jso1919@gmail.com>
2022-02-17 16:03:43 -0500

    added readme image and info

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'; // importing bootstrap styles
+import './index.css'; // importing my styles
 
 ReactDOM.render(
   <React.StrictMode>
--- src/scenes/Login.js
+++ src/scenes/Login.js
@@ -3,6 +3,8 @@ import { Link, useNavigate } from 'react-router-dom'
 import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { app } from '../ConnectAuth'
 
+import { Button, CardGroup, Card, Carousel, ButtonGroup } from 'react-bootstrap'
+
 export default function Login({ setUser, user }) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -72,6 +74,76 @@ export default function Login({ setUser, user }) {
         }}>
         Sign in with Google
       </button>
+      <Button>test</Button>
+      <CardGroup className='container'>
+        <Card>
+          <Card.Img variant='top' src='https://picsum.photos/100/200' />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className='text-muted'>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant='top' src='https://picsum.photos/100/200' />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>This card has supporting text below as a natural lead-in to additional content. </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className='text-muted'>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant='top' src='https://picsum.photos/100/200' />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the
+              first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className='text-muted'>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+
+      <Carousel>
+        <Carousel.Item>
+          <img className='d-block w-100' src='https://picsum.photos/200/300' alt='First slide' />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className='d-block w-100' src='https://picsum.photos/200/300' alt='Second slide' />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className='d-block w-100' src='https://picsum.photos/200/300' alt='Third slide' />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+      <ButtonGroup aria-label='Basic example'>
+        <Button variant='secondary'>Left</Button>
+        <Button variant='secondary'>Middle</Button>
+        <Button variant='secondary'>Right</Button>
+      </ButtonGroup>
       <p>
         Not a user? <Link to='/signup'>Sign Up</Link>
       </p>
